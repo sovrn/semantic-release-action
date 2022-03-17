f9ab0177d221405594d8b61d9e31731bf5041951
Chris Prokopiak <cprokopiak@sovrn.com>
2022-03-16 20:51:32 -0600

    feat: rely on local semantic release for action
    Remove the settings to install versions of semantic release, plugins,
    and extensions. Instead, rely on any repo using this action on having
    an installation of semantic release already in their repo, along with
    configuration.
    
    BREAKING CHANGE: Removed the following inputs to the action:
    `semantic_version`, `branches`, `branch`, `extra_plugins`, `extends`,
    and `working_directory`. The configuration should all live in the repo
    in a `.releaserc` file.

--- src/handleOptions.js
+++ src/handleOptions.js
@@ -1,45 +1,5 @@
 const core = require('@actions/core');
-const stringToJson = require('@cycjimmy/awesome-js-funcs/cjs/typeConversion/stringToJson').default;
 const inputs = require('./inputs.json');
-
-/**
- * Handle Branches Option
- * @returns {{}|{branch: string}}
- */
-exports.handleBranchesOption = () => {
-  const branchesOption = {};
-  const branches = core.getInput(inputs.branches);
-  const branch = core.getInput(inputs.branch);
-
-  core.debug(`branches input: ${branches}`);
-  core.debug(`branch input: ${branch}`);
-
-  const semanticVersion = require('semantic-release/package.json').version;
-  const semanticMajorVersion = Number(semanticVersion.replace(/\..+/g, ''));
-  core.debug(`semanticMajorVersion: ${semanticMajorVersion}`);
-
-  // older than v16
-  if (semanticMajorVersion < 16) {
-    if (!branch) {
-      return branchesOption;
-    }
-
-    branchesOption.branch = branch;
-    return branchesOption;
-  }
-
-  // above v16
-  const strNeedConvertToJson = branches || branch || '';
-
-  if (!strNeedConvertToJson) {
-    return branchesOption;
-  }
-
-  const jsonOrStr = stringToJson('' + strNeedConvertToJson);
-  core.debug(`Converted branches attribute: ${JSON.stringify(jsonOrStr)}`);
-  branchesOption.branches = jsonOrStr;
-  return branchesOption;
-};
 
 /**
  * Handle DryRun Option
@@ -57,19 +17,5 @@ exports.handleDryRunOption = () => {
 
     default:
       return {};
-  }
-};
-
-/**
- * Handle Extends Option
- * @returns {{}|{extends: Array}|{extends: String}}
- */
-exports.handleExtends = () => {
-  const extend = core.getInput(inputs.extends);
-
-  if (extend) {
-    return { extends: extend };
-  } else {
-    return {};
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,37 +1,22 @@
 const core = require('@actions/core');
 const {
-  handleBranchesOption,
   handleDryRunOption,
-  handleExtends,
 } = require('./handleOptions');
 const setUpJob = require('./setUpJob.task');
-const installSpecifyingVersionSemantic = require('./installSpecifyingVersionSemantic.task');
-const preInstall = require('./preInstall.task');
-const cleanupNpmrc = require('./cleanupNpmrc.task');
 const windUpJob = require('./windUpJob.task');
-const inputs = require('./inputs.json');
 
 /**
  * Release main task
  * @returns {Promise<void>}
  */
 const release = async () => {
-  if (core.getInput(inputs.working_directory)) {
-    process.chdir(core.getInput(inputs.working_directory));
-  }
   await setUpJob();
-  await installSpecifyingVersionSemantic();
-  await preInstall(core.getInput(inputs.extra_plugins));
-  await preInstall(core.getInput(inputs.extends));
 
   const semanticRelease = require('semantic-release');
   const result = await semanticRelease({
-    ...handleBranchesOption(),
     ...handleDryRunOption(),
-    ...handleExtends(),
   });
 
-  await cleanupNpmrc();
   await windUpJob(result);
 };
 
